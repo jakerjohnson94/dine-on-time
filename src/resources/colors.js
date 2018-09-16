7163a3d1f3bacac471defe09b2d0b951861a3526
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-09-16 15:44:26 -0400

    init

---
+++ src/resources/colors.js
@@ -0,0 +1,6 @@
+export const appBlue = '#1a3666';
+export const appGreen = '#1a664a';
+export const appBrown = '#664a1a';
+export const appRed = '#661a36';
+export const fontGreyPrimary = '#696969';
+export const fontGreySecondary = '#666666';
