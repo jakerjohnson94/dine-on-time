5b3d4339c980e9e2b6eec25e71b181edd9668f04
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-09-19 16:09:00 -0400

    getting data correctly and properly formatted from redux

--- src/components/RecipeId/RecipeIdContent.jsx
+++ src/components/RecipeId/RecipeIdContent.jsx
@@ -9,42 +9,35 @@ import {
   CardContent,
   Icon,
 } from '@material-ui/core';
-import TimeInput from '../TimeInput';
+import TimeInput from './TimeInput';
 
-import chikpeasImg from '../../resources/images/recipes/sample/ingredients/chikpeas.png';
-import eggImg from '../../resources/images/recipes/sample/ingredients/egg.png';
-import fetaImg from '../../resources/images/recipes/sample/ingredients/feta.png';
-
-import garlicImg from '../../resources/images/recipes/sample/ingredients/garlic.png';
-import gingerImg from '../../resources/images/recipes/sample/ingredients/ginger.png';
-import ketchupImg from '../../resources/images/recipes/sample/ingredients/ketchup.png';
-import nutsImg from '../../resources/images/recipes/sample/ingredients/nuts.png';
-import onionImg from '../../resources/images/recipes/sample/ingredients/onion.png';
-// import powder2Img from '../../resources/images/recipes/sample/ingredients/powder2.png';
-// import spice1Img from '../../resources/images/recipes/sample/ingredients/spice1.png';
-// import spinachImg from '../../resources/images/recipes/sample/ingredients/spinach.png';
-// import tomatoesImg from '../../resources/images/recipes/sample/ingredients/tomatoes.png';
-import recipeImg from '../../resources/images/recipes/sample/sample.jpg';
 import { appBlue, fontGreyPrimary, fontGreySecondary } from '../../resources/colors';
-import fetchRecipeById from '../../redux/recipeAction.js';
+
 const style = {
   header: {
     textAlign: 'center',
     color: appBlue,
   },
   recipeImage: {
-    height: '25vh',
+    height: '40vh',
   },
 
   recipeName: {
     textAlign: 'center',
-    paddingTop: '.7em',
+    fontSize: '1.8em',
+    paddingTop: '.8em',
+    color: appBlue,
   },
   recipeDescription: {
-    fontSize: '.8em',
+    fontSize: '1.1em',
+  },
+  recipeSubtitle: {
+    color: fontGreyPrimary,
+    textAlign: 'center',
+    paddingBottom: '.5em',
   },
   ingredientImg: {
-    height: '4.1em',
+    height: '5em',
     marginBottom: '.0',
   },
   centeredIngredient: {
@@ -54,17 +47,21 @@ const style = {
     color: fontGreySecondary,
     marginTop: '0',
     marginBottom: '.2em',
+    fontSize: '1.3em',
   },
   ingQty: {
     marginTop: '0',
     marginBottom: '.3em',
+    fontSize: '1.3em',
   },
   recipeInfoList: {
     marginTop: '2em',
+    marginBottom: '1.5em',
   },
   infoListTitle: {
     marginBottom: '.2em',
-    fontSize: '.7em',
+    fontSize: '.8em',
+
     color: 'black',
     textAlign: 'center',
   },
@@ -72,10 +69,11 @@ const style = {
     marginTop: '0',
     color: fontGreySecondary,
     textAlign: 'center',
-    fontSize: '.7em',
+    fontSize: '1em',
   },
   infoListIcon: {
     textAlign: 'center',
+    fontSize: '2em',
   },
   timer: {
     display: 'flex',
@@ -85,20 +83,19 @@ const style = {
     alignItems: 'center',
   },
 };
+const formatIngredientList = steps => {
+  const result = [];
+  const isOdd = steps.length % 2 !== 0;
+  let remainder = isOdd ? steps.splice(steps.length - 1, 1) : null;
+  for (let i = 0; i < steps.length; i += 2) {
+    if (steps[i + 1] !== undefined) result.push(steps.slice(i, i + 2));
+  }
+  if (remainder) result.push(remainder);
+  return result;
+};
 
 class recipeIdContent extends Component {
   render() {
-    const handleIngredientFormatting = steps => {
-      const result = [];
-      const isOdd = steps.length % 2 !== 0;
-      let remainder = isOdd ? steps.splice(steps.length - 1, 1) : null;
-      for (let i = 0; i < steps.length; i += 2) {
-        if (steps[i + 1] !== undefined) result.push(steps.slice(i, i + 2));
-      }
-      if (remainder) result.push(remainder);
-      return result;
-    };
-    console.log(this.props);
     return (
       <Grid alignContent="center" alignItems="center" justify="center" container>
         <Grid item xs={12}>
@@ -113,8 +110,13 @@ class recipeIdContent extends Component {
               </Grid>
 
               <Grid item xs={12}>
-                <Typography style={style.recipeName} gutterBottom variant="headline">
+                <Typography style={style.recipeName} variant="headline">
                   {this.props.recipe.title || 'RECIPE NOT FOUND'}
+                </Typography>
+              </Grid>
+              <Grid item xs={12}>
+                <Typography style={style.recipeSubtitle} gutterBottom variant="subheading">
+                  {this.props.recipe.subtitle || null}
                 </Typography>
               </Grid>
               <Grid item xs={7}>
@@ -123,7 +125,7 @@ class recipeIdContent extends Component {
                     {this.props.recipe.description || '-'}
                   </Typography>
                   <Grid item style={style.recipeInfoList} xs={12}>
-                    <Grid container justify="space-around">
+                    <Grid container justify="space-between">
                       <Grid style={style.infoListIcon} item xs={4}>
                         <Icon>alarm_on</Icon>
                       </Grid>
@@ -153,9 +155,7 @@ class recipeIdContent extends Component {
                         <p style={style.infoListInfo}>{this.props.recipe.servings}</p>
                       </Grid>
                       <Grid item xs={4}>
-                        <p style={style.infoListInfo}>
-                          Est. {this.props.recipe.estimatedCalories} cals{' '}
-                        </p>
+                        <p style={style.infoListInfo}>{this.props.recipe.calories} cals </p>
                       </Grid>
                     </Grid>
 
@@ -172,26 +172,45 @@ class recipeIdContent extends Component {
                 <CardActionArea style={{ pointerEvents: 'none' }}>
                   <CardContent style={{ padding: '.3em' }}>
                     <Grid container>
-                      {/* ingredient images */}
-                      <Grid style={style.centeredIngredient} item xs={6}>
-                        <img style={style.ingredientImg} src={this.props.ingImg1 || chikpeasImg} />
-                      </Grid>
-                      <Grid style={style.centeredIngredient} item xs={6}>
-                        <img style={style.ingredientImg} src={this.props.ingImg2 || eggImg} />
-                      </Grid>
+                      {formatIngredientList(this.props.ingredients).map(ing => (
+                        <React.Fragment key={ing[0].name}>
+                          <Grid style={style.centeredIngredient} item xs={6}>
+                            <img style={style.ingredientImg} key={ing[0].name} src={ing[0].img} />
+                          </Grid>
+                          {ing[1] !== undefined ? (
+                            <Grid style={style.centeredIngredient} item xs={6}>
+                              <img
+                                style={style.ingredientImg}
+                                key={ing[1].img}
+                                src={ing[1].img || null}
+                              />
+                            </Grid>
+                          ) : null}
 
-                      {/* {ingredient names} */}
-                      <Grid style={style.centeredIngredient} item xs={6}>
-                        <p style={style.ingQty}>{'1 15oz can '}</p>
-                        <p style={style.ingName}> {'Chikpeas'}</p>
-                      </Grid>
-                      <Grid style={style.centeredIngredient} item xs={6}>
-                        <p style={style.ingQty}>{'2 '}</p>
-                        <p style={style.ingName}>{'Pasture-Raised Eggs '}</p>
-                      </Grid>
-
-                      {this.props.ingredients.map(ing => (
-                        <div>{ing.name}</div>
+                          {/* {ingredient names} */}
+                          <Grid
+                            style={style.centeredIngredient}
+                            item
+                            xs={ing[1] !== undefined ? 6 : 12}
+                          >
+                            <p style={style.ingQty} key={ing[0].quantity}>
+                              {ing[0].quantity}
+                            </p>
+                            <p style={style.ingName} key={ing[0].name}>
+                              {ing[0].name}
+                            </p>
+                          </Grid>
+                          {ing[1] !== undefined ? (
+                            <Grid style={style.centeredIngredient} item xs={6}>
+                              <p style={style.ingQty} key={ing[1].quantity}>
+                                {ing[1].quantity}
+                              </p>
+                              <p style={style.ingName} key={ing[1].name}>
+                                {ing[1].name}
+                              </p>
+                            </Grid>
+                          ) : null}
+                        </React.Fragment>
                       ))}
                     </Grid>
                   </CardContent>
@@ -212,30 +231,3 @@ const mapStateToProps = state => {
 };
 
 export default connect(mapStateToProps)(recipeIdContent);
-
-// {this.handleIngredientFormatting(this.props.recipe.ingredients).map(
-//   ingredient => (
-//     <React.Fragment>
-//       <Grid style={style.centeredIngredient} item xs={6}>
-//         <img
-//           style={style.ingredientImg}
-//           src={ingredient[0].img || chikpeasImg}
-//         />
-//       </Grid>
-
-//       <Grid style={style.centeredIngredient} item xs={6}>
-//         <img style={style.ingredientImg} src={ingredient[1].img || eggImg} />
-//       </Grid>
-
-//       {/* {ingredient names} */}
-//       <Grid style={style.centeredIngredient} item xs={6}>
-//         <p style={style.ingQty}>{ingredient[0].quantity}</p>
-//         <p style={style.ingName}> {ingredient[0].name}</p>
-//       </Grid>
-//       <Grid style={style.centeredIngredient} item xs={6}>
-//         <p style={style.ingQty}>{ingredient[1].quantity || null}</p>
-//         <p style={style.ingName}>{ingredient[1].name || null}</p>
-//       </Grid>
-//     </React.Fragment>
-//   )
-// )}
