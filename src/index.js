a7ed5fc6f6d137eb2c4646321b017126e0715549
tgentry300 <taylor.gentry300@gmail.com>
2018-10-01 16:02:15 -0400

    deploying

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App.jsx';
 import registerServiceWorker from './registerServiceWorker';
 
-import { Router } from 'react-router-dom';
+import { Router, BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { store } from './redux/store';
 import history from './history';
