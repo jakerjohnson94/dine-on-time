fe25956a32cee9b0ce476929f6f5b8ff3399b7e2
Jakerjohnson94 <jacobrichardjohnson94@gmail.com>
2018-10-03 15:15:16 -0400

    root page infographic

--- src/components/Root/CannotFindQR.jsx
+++ src/components/Root/CannotFindQR.jsx
@@ -1,26 +1,20 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import {
-  Button,
-  Dialog,
-  DialogActions,
-  DialogContent,
-  DialogTitle
-} from '@material-ui/core';
+import { Button, Dialog, DialogActions, DialogContent, DialogTitle } from '@material-ui/core';
 import { fontGreyPrimary, fontGreySecondary, appBlue } from '../../resources/colors';
 
 const style = {
-    buttonStyling: {
-        textTransform: 'none',
-        color: fontGreySecondary,
-        fontSize: '0.6rem',
-        marginTop: '-2rem'
-    }
-}
+  buttonStyling: {
+    textTransform: 'none',
+    width: '100%',
+    color: fontGreySecondary,
+    marginTop: '5em',
+  },
+};
 
 class CannotFindQR extends Component {
   state = {
-    isOpen: false
+    isOpen: false,
   };
 
   onClick = () => {
